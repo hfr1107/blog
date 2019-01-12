8de95f0339b5b271aa84bb1ef559b94b70666282
scemsjyd <scemsjyd@vip.qq.com>
2019-01-12 19:33:42 +0800

    Site updated: 2019-01-12 19:33:42

--- photos/ins.js
+++ photos/ins.js
@@ -110,62 +110,33 @@
         addMask($videoImg[i]);
       }
     };
-//    var render = function render(res) {
-//      var ulTmpl = "";
-//      for (var j = 0, len2 = res.list.length; j < len2; j++) {
-//        var data = res.list[j].arr;
-//        var liTmpl = "";
-//        for (var i = 0, len = data.link.length; i < len; i++) {
-//          var minSrc = 'http://litten.me/ins-min/' + data.link[i] + '.min.jpg';
-//          var src = 'http://litten.me/ins/' + data.link[i];
-//          var type = data.type[i];
-//          var target = src + (type === 'video' ? '.mp4' : '.jpg');
-//          src += '.jpg';
-//
-//          liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
-//                <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
-//                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
-//                </a>\
-//                <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
-//            </figure>';
-//        }
-//        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '<em>' + data.month + '月</em></h1>\
-//        <ul class="img-box-ul">' + liTmpl + '</ul>\
-//        </section>';
-//      }
-//      document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope="" itemtype="http://schema.org/ImageGallery">' + ulTmpl + '</div>';
-//      createVideoIncon();
-//      _view2.default.init();
-//    };
-var render = function render(res) {
-          var ulTmpl = "";
-          for (var j = 0, len2 = res.list.length; j < len2; j++) {
-          var data = res.list[j].arr;
-          var liTmpl = "";
-          for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = 'https://raw.githubusercontent.com/scemsjyd/scemsjyd.github.io/master/min_photos/' + data.link[i];
-
-          var src = 'https://raw.githubusercontent.com/scemsjyd/scemsjyd.github.io/master/max-photos/' + data.link[i];
-
+    var render = function render(res) {
+      var ulTmpl = "";
+      for (var j = 0, len2 = res.list.length; j < len2; j++) {
+        var data = res.list[j].arr;
+        var liTmpl = "";
+        for (var i = 0, len = data.link.length; i < len; i++) {
+          var minSrc = 'http://litten.me/ins-min/' + data.link[i] + '.min.jpg';
+          var src = 'http://litten.me/ins/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
-          src += '';
+          src += '.jpg';
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
-                <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
+                <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
                   <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
         }
-        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
+        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '<em>' + data.month + '月</em></h1>\
         <ul class="img-box-ul">' + liTmpl + '</ul>\
         </section>';
-        }
-        document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope="" itemtype="http://schema.org/ImageGallery">' + ulTmpl + '</div>';
-        createVideoIncon();
-        _view2.default.init();
-        };
+      }
+      document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope="" itemtype="http://schema.org/ImageGallery">' + ulTmpl + '</div>';
+      createVideoIncon();
+      _view2.default.init();
+    };
 
     var replacer = function replacer(str) {
       var arr = str.split("/");
@@ -198,7 +169,7 @@ var render = function render(res) {
     function loadData(success) {
       if (!searchData) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', './data.json?t=' + +new Date(), true);
+        xhr.open('GET', './ins.json?t=' + +new Date(), true);
 
         xhr.onload = function() {
           if (this.status >= 200 && this.status < 300) {
