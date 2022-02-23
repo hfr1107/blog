b390cf082c5a544f185f7edc77adac8d7018a47b
Adam.Jin <scemsjyd@vip.qq.com>
2022-02-23 16:57:11 +0800

    重新生成站点

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup hugo server --bind 0.0.0.0 -p 20002 -w --environment production >nohup.out  2>&1 &
+nohup hugo server -D --bind 0.0.0.0 -p 20002 -w --disableLiveReload --environment production >nohup.out  2>&1 &
